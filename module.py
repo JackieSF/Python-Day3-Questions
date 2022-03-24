029e98cab4096391276bf13b4a9e2ae21541058b
JackieSF <jaclanesf@gmail.com>
2022-03-24 10:35:07 -0400

    first commit

---
+++ module.py
@@ -0,0 +1,18 @@
+L = float(input('Enter the length of a house: '))
+W = float(input('Enter the width of house: '))
+
+square_feet = L * W 
+
+print("The square footage of the house is: %2f " % square_feet)
+
+
+
+# circle
+
+import math
+
+radius = float(input("Enter the radius of the circle : "))
+
+circumference = 2 * math.pi * radius
+
+print("The circumference of the circle is : %.2f" % circumference)
